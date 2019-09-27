f25fe421254d4506743113912e4d6306eba9c17d
Ofir Knimach <o.knimach@hotmail.com>
2019-09-27 17:12:12 +1000

    Added Post Create and Update functionality through usage of classbased views

--- django_webapp/blog/views.py
+++ django_webapp/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView
+    )
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
@@ -8,6 +15,33 @@ def home(request):
         'posts': Post.objects.all()
     };
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'    # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 
 def about(request):
     return render(request, 'blog/about.html', { 'title': 'About Page' })
